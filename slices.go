323b1e2641d0a50f95252aa16e632f57b35c524e
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-05 15:32:54 +0000

    slices

--- slices.go
+++ slices.go
@@ -16,13 +16,21 @@ func main() {
 
 	fmt.Println(slice1)
 
+	slice1 = append(slice1, 6, 7)
+	fmt.Println(slice1)
 
-slice1 = append(slice1, 6,7)
-fmt.Println(slice1)
+	sliceCopy := make([]int, len(slice1))
+	copy(sliceCopy, slice1)
+	fmt.Println("Copy:", sliceCopy)
 
+	// var nilSlice []int
 
-sliceCopy := make([]int, len(slice1))
-copy(sliceCopy, slice1)
-fmt.Println(sliceCopy)
+	for i, v := range slice1 {
+		fmt.Println(i, v)
+	}
+	fmt.Println("Element at index 3:", slice1[3])
+
+	slice1[3] = 50
+	fmt.Println("Element at index 3:", slice1[3])
 
 }
