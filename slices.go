da9c87ee5475fdcdd378dfaae3418265fa9ce9ca
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-05 15:26:09 +0000

    slices

---
+++ slices.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var sliceName [] ElementType
+
+	// var numbers []int
+	// var numbers1 = []int{1,2,3}
+
+	// numbers2 = []int{9,8,7}
+
+	a := [5]int{1, 2, 3, 4, 5}
+	slice1 := a[1:4]
+
+	fmt.Println(slice1)
+
+
+slice1 = append(slice1, 6,7)
+fmt.Println(slice1)
+
+
+sliceCopy := make([]int, len(slice1))
+copy(sliceCopy, slice1)
+fmt.Println(sliceCopy)
+
+}
