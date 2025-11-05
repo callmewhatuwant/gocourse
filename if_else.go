1f598a343e124c979bd22ccf761112325515130e
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-05 12:44:01 +0000

    if_else

--- if_else.go
+++ if_else.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 
 	// age := 25
 
@@ -17,15 +17,34 @@ func main()  {
 
 	// }
 
-	score := 85
+	// score := 85
 
-	if score >= 90 {
-		fmt.Println("Grade A")
-	}else if score >= 80 {
-		fmt.Println("Grade B")
-	}else if score >= 70 {
-		fmt.Println("Grade C")
-	}else {
-		fmt.Println("Grade D")
+	// if score >= 90 {
+	// 	fmt.Println("Grade A")
+	// }else if score >= 80 {
+	// 	fmt.Println("Grade B")
+	// }else if score >= 70 {
+	// 	fmt.Println("Grade C")
+	// }else {
+	// 	fmt.Println("Grade D")
+	// }
+
+	// num := 15
+	// if num %2 == 0 {
+	// 		if num % 3 == 0 {
+	// 			fmt.Println("Number is divisible by both 2 and 3.")
+	// 		} else {
+	// 			fmt.Println("Number is divisible by 2 but not 3.")
+	// 		}
+	// } else {
+	// 	fmt.Println("Number is not divisible by 2.")
+	// }
+
+	if 10%2 == 0 || 5%2 == 0 {
+		fmt.Println("Eihter Numbers are even")
+	}
+
+	if 10%2 == 0 && 6%2 == 0 {
+		fmt.Println("Both Numbers are even")
 	}
 }
