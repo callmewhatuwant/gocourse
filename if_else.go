07140c21e036c596eb9e3cc4b71f8eeaadab09ca
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-05 11:04:02 +0000

    if_else

---
+++ if_else.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	// age := 25
+
+	// if age >= 18 {
+	// 	fmt.Println("You are an adult.")
+	// }
+	// temperature := 25
+	// if temperature >= 30 {
+	// 	fmt.Println("It's hot outside.")
+	// } else {
+	// 	fmt.Println("It's cool outside.")
+
+	// }
+
+	score := 85
+
+	if score >= 90 {
+		fmt.Println("Grade A")
+	}else if score >= 80 {
+		fmt.Println("Grade B")
+	}else if score >= 70 {
+		fmt.Println("Grade C")
+	}else {
+		fmt.Println("Grade D")
+	}
+}
