02845d313b39da42348a8ca239ab97d04f79f482
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-07 14:40:28 +0000

    maps

--- maps.go
+++ maps.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+)
 
 func main() {
 
@@ -18,5 +21,46 @@ func main() {
 	myMap := make(map[string]int)
 
 	fmt.Println(myMap)
+
+	myMap["key1"] = 9
+	myMap["code"] = 18
+
+	fmt.Println(myMap)
+	fmt.Println(myMap["key1"])
+	fmt.Println(myMap["key"])
+	myMap["code"] = 21
+	fmt.Println(myMap)
+
+	delete(myMap, "key1")
+	fmt.Println(myMap)
+
+	myMap["key1"] = 9
+	myMap["key2"] = 10
+	myMap["key3"] = 11
+	fmt.Println(myMap)
+
+	// clear(myMap)
+	// fmt.Println(myMap)
+
+	// value, unkownvalue := myMap ["key1"]
+	_, unkownvalue := myMap ["key1"]
+	// fmt.Println(value)
+	fmt.Println(unkownvalue)
+
+
+	myMap2 := map[string]int{"a":1, "b":2}
+	myMap3 := map[string]int{"a":1, "b":2}
+	fmt.Println(myMap2)
+
+	if maps.Equal(myMap2,myMap3){
+		fmt.Println("map 1 and 2 are equal")
+	}else{
+		fmt.Println("maps are not equal")
+	}
+
+	for k,v := range myMap3{
+		fmt.Println(k,v)
+	}
+
 
 }
