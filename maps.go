85266cdd6ac22fe57666bffc828af015b936e2be
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-07 14:19:35 +0000

    maps

--- maps.go
+++ maps.go
@@ -1,7 +1,22 @@
 package main
 
-func main()  {
+import "fmt"
 
-	
-	
+func main() {
+
+	// var mapVariable map[key]valueType
+
+	// mapVariable = make (map[keyType]valueType)
+
+	//  using Map Literal
+
+	// mapVaribale = map[keyType]valueType{
+	// 	key1: value1,
+	// 	key2: value2
+	// }
+
+	myMap := make(map[string]int)
+
+	fmt.Println(myMap)
+
 }
