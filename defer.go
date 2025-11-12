c89df1529853a46485e97886021f6805c52403c3
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-12 13:45:59 +0000

    defer

---
+++ defer.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	process(10)
+
+}
+
+func process(i int) {
+	defer fmt.Println("Deferred i value", i)
+	defer fmt.Println("Second deferred statement executed")
+	defer fmt.Println("Third deferred statement executed")
+	i++
+	fmt.Println("Normal execution statement")
+	fmt.Println("value of i:", i)
+}
