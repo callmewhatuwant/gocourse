4760cf21f36f770df7f7b1745190ef35a84acff2
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-12 12:22:14 +0000

    functions

--- functions.go
+++ functions.go
@@ -4,11 +4,32 @@ import "fmt"
 
 func main() {
 
-	// funx <name>(parameters list)returnType {
+	// funx <name> (parameters list) returnType {
 	// code block
 	// return value
 	// }
 
-fmt.Println("")
+	//sum := add(1, 2)
+	fmt.Println(add(2, 3))
 
+	// func ()  {
+	// 	fmt.Println("Anonymous Function")
+	// }()
+
+	greet := func() {
+		fmt.Println("Anonymous Function")
+	}
+
+	greet()
+
+	operation := add
+
+	result := operation(3, 5)
+
+	fmt.Println(result)
+
+}
+
+func add(a, b int) int {
+	return a + b
 }
