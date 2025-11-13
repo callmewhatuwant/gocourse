603cb77d81cf22432f1083f5353feeaec65530c5
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-13 14:21:45 +0000

    closures

--- intermediate/closures.go
+++ intermediate/closures.go
@@ -3,14 +3,26 @@ package main
 import "fmt"
 
 func main() {
- 
+
 	sequence := adder()
 	fmt.Println(sequence())
 	fmt.Println(sequence())
 	fmt.Println(sequence())
-
-	fmt.Println("test")
 	fmt.Println(sequence())
+
+	susbtractor := func() func(int) int {
+		countdown := 99
+		return func(x int) int {
+			countdown -= x
+			return countdown
+		}
+	}()
+
+	fmt.Println(susbtractor(1))
+	fmt.Println(susbtractor(2))
+	fmt.Println(susbtractor(3))
+	fmt.Println(susbtractor(4))
+	fmt.Println(susbtractor(5))
 
 }
 
