e1688801284476692ddd245918d211b15041d28e
callmewhatuwant <107889691+callmewhatuwant@users.noreply.github.com>
2025-11-27 15:11:12 +0000

    methods

--- intermediate/methods.go
+++ intermediate/methods.go
@@ -1,7 +1,32 @@
 package main
 
-func main()  {
+import "fmt"
 
-	
-	
+type Rectangle struct {
+	length float64
+	width  float64
+}
+
+// Method with value receiver
+
+func (r Rectangle) Area() float64 {
+	return r.length * r.width
+}
+
+// Method with pointer receiver
+
+func (r *Rectangle) Scale(factor float64) {
+	r.length *= factor
+	r.width *= factor
+}
+
+func main() {
+
+	rect := Rectangle{length: 10, width: 9}
+	area := rect.Area()
+	fmt.Println(area)
+	rect.Scale(2)
+	area = rect.Area()
+	fmt.Println(area)
+
 }
